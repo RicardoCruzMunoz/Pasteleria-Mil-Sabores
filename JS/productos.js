3ba0388b6c6ef6295b69789674a28cb05d786111
RicardoCruzMunoz <ri.cruzm@duocuc.cl>
2025-09-10 12:47:25 -0300

    productos 2

---
+++ JS/productos.js
@@ -0,0 +1,9 @@
+const productos = [
+    ["TC001", "Tortas Cuadradas", "Torta Cuadrada de Chocolate", "$45.000", "imagenes/pasteles/cuadrada_chocolate.jpg", "Deliciosa torta de chocolate con capas de ganache y un toque de avellanas. Personalizable con mensajes especiales."],
+    ["TC002", "Tortas Cuadradas", "Torta Cuadrada de Frutas", "$50.000", "imagenes/pasteles/cuadrada_frutas.jpg", "Una mezcla de frutas frescas y crema chantilly sobre un suave bizcocho de vainilla, ideal para celebraciones"],
+    ["TT001", "Tortas Circulares", "Torta Circular de Vainilla", "$40.000", "imagenes/pasteles/circular_vainilla.png", "Bizcocho de vainilla clásico relleno con crema pastelera y cubierto con un glaseado dulce, perfecto para cualquier ocasión."],
+    ["TT002", "Tortas Circulares", "Torta Circular de Manjar", "$42.000", "imagenes/pasteles/circular_manjar.jpg", "Torta tradicional chilena con manjar y nueces, un deleite para los amantes de los sabores dulces y clásicos."],
+    ["PI001", "Postres Individuales", "Mousse de Chocolate", "$5.000", "imagenes/pasteles/mousse_chocolate.jpg", "Postre individual cremoso y suave, hecho con chocolate de alta calidad, ideal para los amantes del chocolate"],
+    ["PI002", "Postres Individuales", "Torta sin Azúcar de Naranja", "$5.500", "imagenes/pasteles/naranja_sin_azucar.jpg", "Torta ligera y deliciosa, endulzada naturalmente, ideal para quienes buscan opciones más saludables"],
+];
+
